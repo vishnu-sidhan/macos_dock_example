76004cd49ef4fb26c445c8a000f42d4350a48b0d
vishnusidhan <vsidhan@gmail.com>
2024-11-21 17:43:58 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:macos_dock_example/dock.dart';
+import 'package:macos_dock_example/docktile.dart';
 
 void main() {
   runApp(const MainApp());
@@ -7,12 +9,25 @@ void main() {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
+  final Map<String, IconData> items = const {
+    "Contacts": Icons.person,
+    "Messages": Icons.message,
+    "Calls": Icons.call,
+    "Camera": Icons.camera,
+    "Gallery": Icons.photo,
+  };
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Dock(
+            tiles: items.entries
+                .map((v) => DockTileModal(icon: v.value, title: v.key))
+                .toList(),
+          ),
         ),
       ),
     );
